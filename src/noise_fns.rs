e7dd143227e6e6d8c8e83ddb6baf60b407088fd9
ScottyThePilot <scotty.codes@gmail.com>
2022-01-19 17:25:58 -0500

    NoiseFn composing methods

--- src/noise_fns.rs
+++ src/noise_fns.rs
@@ -25,6 +25,194 @@ mod transformers;
 /// * Combining the output values from two noise functions in various ways.
 pub trait NoiseFn<T, const DIM: usize> {
     fn get(&self, point: [T; DIM]) -> f64;
+
+    fn abs(self) -> Abs<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        Abs::new(self)
+    }
+
+    fn add<Other>(self, other: Other) -> Add<T, Self, Other, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>,
+    {
+        Add::new(self, other)
+    }
+
+    fn blend<Other, Control>(self, other: Other, control: Control) -> Blend<T, Self, Other, Control, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>,
+        Control: NoiseFn<T, DIM>
+    {
+        Blend::new(self, other, control)
+    }
+
+    fn cache(self) -> Cache<Self>
+    where
+        Self: Sized,
+    {
+        Cache::new(self)
+    }
+
+    fn clamp(self) -> Clamp<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        Clamp::new(self)
+    }
+
+    fn curve(self) -> Curve<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        Curve::new(self)
+    }
+
+    fn displace_xy<XDisplace, YDisplace>(
+        self,
+        x_displace: XDisplace,
+        y_displace: YDisplace,
+    ) -> Displace<Self, XDisplace, YDisplace, (), ()>
+    where
+        Self: NoiseFn<f64, 2> + Sized,
+        XDisplace: NoiseFn<f64, 2>,
+        YDisplace: NoiseFn<f64, 2>,
+    {
+        Displace::new(self, x_displace, y_displace, (), ())
+    }
+
+    fn displace_xyz<XDisplace, YDisplace, ZDisplace>(
+        self,
+        x_displace: XDisplace,
+        y_displace: YDisplace,
+        z_displace: ZDisplace,
+    ) -> Displace<Self, XDisplace, YDisplace, ZDisplace, ()>
+    where
+        Self: NoiseFn<f64, 3> + Sized,
+        XDisplace: NoiseFn<f64, 3>,
+        YDisplace: NoiseFn<f64, 3>,
+        ZDisplace: NoiseFn<f64, 3>,
+    {
+        Displace::new(self, x_displace, y_displace, z_displace, ())
+    }
+
+    fn displace_xyzu<XDisplace, YDisplace, ZDisplace, UDisplace>(
+        self,
+        x_displace: XDisplace,
+        y_displace: YDisplace,
+        z_displace: ZDisplace,
+        u_displace: UDisplace,
+    ) -> Displace<Self, XDisplace, YDisplace, ZDisplace, UDisplace>
+    where
+        Self: NoiseFn<f64, 4> + Sized,
+        XDisplace: NoiseFn<f64, 4>,
+        YDisplace: NoiseFn<f64, 4>,
+        ZDisplace: NoiseFn<f64, 4>,
+        UDisplace: NoiseFn<f64, 4>,
+    {
+        Displace::new(self, x_displace, y_displace, z_displace, u_displace)
+    }
+
+    fn exponent(self) -> Exponent<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        Exponent::new(self)
+    }
+
+    fn max<Other>(self, other: Other) -> Max<T, Self, Other, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>
+    {
+        Max::new(self, other)
+    }
+
+    fn min<Other>(self, other: Other) -> Min<T, Self, Other, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>
+    {
+        Min::new(self, other)
+    }
+
+    fn multiply<Other>(self, other: Other) -> Multiply<T, Self, Other, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>
+    {
+        Multiply::new(self, other)
+    }
+
+    fn negate(self) -> Negate<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        Negate::new(self)
+    }
+
+    fn power<Other>(self, other: Other) -> Power<T, Self, Other, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>
+    {
+        Power::new(self, other)
+    }
+
+    fn rotate_point(self) -> RotatePoint<Self>
+    where
+        Self: Sized,
+    {
+        RotatePoint::new(self)
+    }
+
+    fn scale_bias(self) -> ScaleBias<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        ScaleBias::new(self)
+    }
+
+    fn scale_point(self) -> ScalePoint<Self>
+    where
+        Self: Sized,
+    {
+        ScalePoint::new(self)
+    }
+
+    fn select<Other, Control>(self, other: Other, control: Control) -> Select<T, Self, Other, Control, DIM>
+    where
+        Self: Sized,
+        Other: NoiseFn<T, DIM>,
+        Control: NoiseFn<T, DIM>
+    {
+        Select::new(self, other, control)
+    }
+
+    fn terrace(self) -> Terrace<T, Self, DIM>
+    where
+        Self: Sized,
+    {
+        Terrace::new(self)
+    }
+
+    fn translate_point(self) -> TranslatePoint<Self>
+    where
+        Self: Sized,
+    {
+        TranslatePoint::new(self)
+    }
+
+    fn turbulence<F>(self) -> Turbulence<Self, F>
+    where
+        Self: Sized,
+        F: Default + Seedable,
+    {
+        Turbulence::new(self)
+    }
 }
 
 impl<'a, T, M, const DIM: usize> NoiseFn<T, DIM> for &'a M
